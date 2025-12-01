7e1649b54d14757e4baa3429375f632f058fbec8
alwaysshuvo <mds126258@gmail.com>
2025-12-02 01:24:06 +0600

    fix: update CORS configuration and improve movie retrieval logic

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://movie-matrix.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://movie-matrix10.netlify.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 
